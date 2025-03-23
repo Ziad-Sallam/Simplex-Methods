c525f382646b726423fbe140dbb5ff1fcc4d2424
ziad <ziadsallam1234@gmail.com>
2025-03-23 21:19:52 +0200

    Added steps to simplex method

--- simplex.py
+++ simplex.py
@@ -1,4 +1,6 @@
 import numpy as np
+from tabulate import tabulate
+
 
 class Simplex():
     def __init__(self, A, b, Z , objective):
@@ -16,6 +18,7 @@ class Simplex():
         self.Z = np.array(Z, dtype=float) * -1 # objective vector
         self.Z_final = 0
         self.BV = [i + self.n for i in range(self.m)]  # Basic variables
+        self.steps = ''
 
     def addingSlackVars(self):
         identity = np.eye(self.m)
@@ -51,6 +54,7 @@ class Simplex():
                     factor = self.A[i, pivotCol]
                     self.A[i] -= factor * self.A[pivotRow]
                     self.b[i] -= factor * self.b[pivotRow]
+                    self.addToSteps()
         
             factor = self.Z[pivotCol]
             self.Z -= factor * self.A[pivotRow]
@@ -69,18 +73,50 @@ class Simplex():
 
         return maxValues, self.Z_final, "optimum solution"
 
+
+    def ansSetup(self):
+
+        self.stp = []
+        self.varNames = [f'x{i}' for i in range(0, self.n)]
+        self.varNames.insert(0, '')
+
+        self.stp.append(self.varNames)
+        for i in range(self.m):
+            self.varNames.append(f'S{i}')
+
+        self.varNames.append('RHS')
+        z = ['Z']
+        for i in range(len(self.A[0])):
+            z.append(self.Z[i])
+        z.append(self.Z_final)
+        self.stp.append(z)
+
+    def addToSteps(self):
+
+        for i in range(len(self.A)):
+            x = self.A[i]
+            x = x.tolist()
+            x.insert(0,self.varNames[self.BV[i]+1])
+            x.append(self.b[i])
+            self.stp.append(x)
+        self.steps += tabulate(self.stp, tablefmt='grid') +'\n\n'
+
+
 def main():
-    A = [[2, -5, 1, -1 , 1, 0], [1, 1, 1, 0 , 0, 1]]
-    b = [10, 7]
-    Z = [-3, 4, -2, 1, 0, 0]
-    objective = -1 #min
+    A = [[1, 4], [1, 2]]
+    b = [8, 4]
+    Z = [3, 9]
+    objective = 1 #min
     simplex = Simplex(A, b, Z, objective)
-   # simplex.addingSlackVars()
-    simplex.Z_final = 17
+    simplex.addingSlackVars()
+    simplex.ansSetup()
+
     maxValues, Z_final, status = simplex.method()
 
     print("Max values of variables:", maxValues)
     print("Final value of Z:", Z_final)
     print("Status:", status)
+    print('steps:')
+    print(simplex.steps)
 
-#main()
+main()
